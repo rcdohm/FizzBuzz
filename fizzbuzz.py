9e5db40de354e4222c4b481ff56175bff5071d5b
Ryland Dohm <rylanddohm@gmail.com>
2024-03-08 16:14:08 -0600

    added comments

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,11 @@
 def main():
-    for i in range(1,101):
-        if i % 3 == 0 and i % 5 ==0:
+    for i in range(1,101): #iterate from 1 to 100
+        if i % 3 == 0 and i % 5 ==0: #check to see if number is evenly divisable by 3 or 5, if so print FizzBuzz
             print("FizzBuzz")
-        elif i % 3 == 0:
+        elif i % 3 == 0: #check to see if number is evenly divisable by 3, if so print Fizz
             print("Fizz")
-        elif i % 5 == 0:
+        elif i % 5 == 0: #check to see if number is evenly divisable by 5, if so print Buzz
             print("Buzz")
-        else:
+        else: #if number is not divisable by 3,5, or both, then just print the number
             print(i)
 main()
